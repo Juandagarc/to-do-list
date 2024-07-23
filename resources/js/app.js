e9353d0190476056bfeb64ef4910bfffa73caf4e
Juandagarc <juandagarce@gmail.com>
2024-07-23 01:03:42 -0500

    vue conection and auth

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,26 +3,26 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-
-import './bootstrap';
 import { createApp } from 'vue';
-import HomePage from './Pages/HomePage.vue';
+import HomePage from './pages/HomePage.vue';
+import Navbar from "./components/navbars/Navbar.vue";
 /**
  * Next, we will create a fresh Vue application instance. You may then begin
- * registering Pages with the application instance so they are ready
+ * registering pages with the application instance so they are ready
  * to use in your application's views. An example is included for you.
  */
 
 const app = createApp({});
 
 app.component('home-page', HomePage);
+app.component('navbar', Navbar);
 
 /**
  * The following block of code may be used to automatically register your
- * Vue Pages. It will recursively scan this directory for the Vue
- * Pages and automatically register them with their "basename".
+ * Vue pages. It will recursively scan this directory for the Vue
+ * pages and automatically register them with their "basename".
  *
- * Eg. ./Pages/ExampleComponent.vue -> <example-component></example-component>
+ * Eg. ./pages/ExampleComponent.vue -> <example-component></example-component>
  */
 
 // Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
--- resources/views/principal/home.blade.php
+++ resources/views/principal/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('navbar')
+    <navbar></navbar>
+@endsection
+
 @section('content')
     <div class="w-full">
         <home-page></home-page>
